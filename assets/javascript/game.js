09fd9fd660efd80e00f57547a3a105bf4e8aff98
M L <ml@Ms-MacBook-Pro.home>
2018-10-09 19:10:07 -0400

    work in progress

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,57 +1,51 @@
 //$document.ready(function(){
 
-    //create an array with one word
     
-    var masterArr=[['q','u','e'],['g','e']];
+    
+    var masterArr=[['q','u','e', 'e', 'n',], ['l', 'o', 'v', 'e']];
     var correctGuess =[];
     var inCorrectGuess =[];
-    var count = 0;
-    var currentWord = 0;
+    var count = 0; // number of turns/words in the game
+    var loss = 0// number of times user makes wrong guess
+    var endOfGame=masterArr.length;
+    var currentWord = masterArr[count];
+    
+    // is it the begining of the game or the end of the game?
+     
+     //create an array with the same length as current word to hold the correct guesses
+      
+      for(var i = 0; i< currentWord.length; i++){
+        correctGuess[i]='_'
+      }
+      
 
-   
-    
-    //create an array with the same length as current word to hold the correct guesses
-    for(var i = 0; i<masterArr.length; i++){ // main for loop
-        currentWord = masterArr[i];
-       
-        for(var j = 0; j<currentWord.length; j++){
-            correctGuess[j]= '-'
-        }
-        //correctGuess =[]; // where does this go?
-        
-       
-    
-        
-        // Have user enter a letter
-        document.onkeyup = function(event){
+      document.onkeyup = function(event){
         var userInput = String.fromCharCode(event.keyCode).toLowerCase(); 
-        console.log(currentWord);
-        //look to see if user entry mathes letter in word
+    
+        ////look to see if user entry matches letter in currentWord
         var letterPos = currentWord.indexOf(userInput);
+       
 
         //if there is a match replace the later with '-' in the word array and add it in the correct position in the correctGuess array
         //if there is no match add the letter to the inCorrectGuess array ( for latter and increase the count )
-        if(userInput === masterArr[letterPos]){
-            masterArr.splice(letterPos, 1, '_');
-            correctGuess[letterPos] = userInput;
-            
+        if(userInput === currentWord[letterPos]){
+            currentWord.splice(letterPos, 1, '_');
+            correctGuess[letterPos] = userInput;   
         }else{
-            //ensure that entry does not aleady exist in inCorrectGuess
-            inCorrectGuess.push(userInput);
-            count++;
-            // console.log(inCorrectGuess, count);
-            
-            
-        }//end of else
+            if(inCorrectGuess.indexOf(userInput)===-1){
+                inCorrectGuess.push(userInput);
+                loss++;
+
+            }
+        }
+        console.log(currentWord);
+        console.log(correctGuess);
+        console.log(inCorrectGuess);
+    }
+
         
-        
-        }//end of event function
+     
+      
 
-}//end of main for loop
-
-
-
-
-
-    
+  
 //})//end of $document.ready
