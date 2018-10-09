4d862849ff2da983828ba1995495baa9a8bdcae4
M L <ml@Ms-MacBook-Pro.home>
2018-10-09 17:32:38 -0400

    reworking flow and structure of javacript

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,45 +6,48 @@
     var correctGuess =[];
     var inCorrectGuess =[];
     var count = 0;
+    var currentWord = 0;
+
+   
     
     //create an array with the same length as current word to hold the correct guesses
-    for(var i = 0; i<masterArr.length; i++){
+    for(var i = 0; i<masterArr.length; i++){ // main for loop
         currentWord = masterArr[i];
-        console.log(currentWord);
-        for(var j = 0; i<currentWord.length; j++){
-            correctGuess[j] = '_'
+       
+        for(var j = 0; j<currentWord.length; j++){
+            correctGuess[j]= '-'
         }
-        //console.log(correctGuess);
-    }
+        //correctGuess =[]; // where does this go?
+        
+       
     
-   
-     
-    // Have user enter a letter
-    document.onkeyup = function(event){
-    var userInput = String.fromCharCode(event.keyCode).toLowerCase(); 
+        
+        // Have user enter a letter
+        document.onkeyup = function(event){
+        var userInput = String.fromCharCode(event.keyCode).toLowerCase(); 
+        console.log(currentWord);
+        //look to see if user entry mathes letter in word
+        var letterPos = currentWord.indexOf(userInput);
 
-    //look to see if user entry mathes letter in word
-    var letterPos = masterArr.indexOf(userInput);
-
-    //if there is a match replace the later with '-' in the word array and add it in the correct position in the correctGuess array
-    //if there is no match add the letter to the inCorrectGuess array ( for latter and increase the count )
-    if(userInput === masterArr[letterPos]){
-        masterArr.splice(letterPos, 1, '_');
-        correctGuess[letterPos] = userInput;
-        console.log(masterArr);
-        console.log(correctGuess);
-    }else{
-        //ensure that entry does not aleady exist in inCorrectGuess
-        // if(inCorrectGuess.indexOf(userInput)===-1){
+        //if there is a match replace the later with '-' in the word array and add it in the correct position in the correctGuess array
+        //if there is no match add the letter to the inCorrectGuess array ( for latter and increase the count )
+        if(userInput === masterArr[letterPos]){
+            masterArr.splice(letterPos, 1, '_');
+            correctGuess[letterPos] = userInput;
+            
+        }else{
+            //ensure that entry does not aleady exist in inCorrectGuess
             inCorrectGuess.push(userInput);
             count++;
-            //console.log(inCorrectGuess, count);
-        // }
+            // console.log(inCorrectGuess, count);
+            
+            
+        }//end of else
         
-    }//end of else
-    
-    
-    }//end of event function
+        
+        }//end of event function
+
+}//end of main for loop
 
 
 
