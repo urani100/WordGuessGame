02fef85909b183477d32cb2067d240c621bae020
M L <ml@Ms-MacBook-Pro.home>
2018-10-12 16:17:34 -0400

    work in progress

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,120 +1,159 @@
 $(document).ready(function(){
 
-  
-    var masterArr=[
+var obj ={
+    masterArr : [
         { name: "qe", pic: "assets/images/pic2.png", band:"Q"},
-        { name: "ko", pic:"assets/images/pic3.png", band:"Queen"}
+        { name: "qe", pic:"assets/images/pic3.png", band:"Queen"}
     
-    ]
-    var correctGuess =[];
-    var inCorrectGuess =[];
-    var target = Math.floor(Math.random() * masterArr.length);
-    var currentWord = masterArr[target]["name"].split(""); 
-    var loss = 0;// number of times user makes wrong guess
-    var win = 0;
+    ],
+    correctGuess : [],
+    inCorrectGuess: [],
+    currentWord : [], 
+    currantWordUpdate: [],
+    //currentWord = masterArr[target]["name"].split(""), 
+    loss :0,// number of times user makes wrong guess
+    win: 0,
+    //target : Math.floor(Math.random() * this.masterArr.length),
 
-
-    //settirng up correctGuess with dashes with correct ni=umnber of letters
-    for(var i = 0; i< currentWord.length; i++){
-            correctGuess[i] = ' - ';
+    setGame : function(){
+        
+        //stillPlaying = false;
+        //var correctGuess =[];
+        //var inCorrectGuess =[];
+        
+        target = Math.floor(Math.random() * this.masterArr.length)
+        this.currentWord = this.masterArr[target]["name"].split("");
+        this.currentWordUpdate =this.masterArr[target]["name"].split("");
+        for(var i = 0; i< this.currentWord.length; i++){
+            this.correctGuess[i] = ' - ';
         }
 
-    var setGame = function(){
-        stillPlaying = false;
-        correctGuess =[];
-        inCorrectGuess =[];
-        var target = Math.floor(Math.random() * masterArr.length);
-        var currentWord = masterArr[target]["name"].split(""); 
-        for(var i = 0; i< currentWord.length; i++){
-            correctGuess[i] = ' - ';
+    }, //end of setGame 
+
+    settingGuessArray : function(){
+           var correct_guess = this.correctGuess;
+           var new_correct_guess = $("<h3>");
+           new_correct_guess.addClass("correct");
+           $('#correct').html(correct_guess);
+        },
+        //end of settingGuessArray
+    letterCheck: function(){
+        var userInput = String.fromCharCode(event.keyCode).toLowerCase(); 
+        var letterPos = this.currentWordUpdate.indexOf(userInput);
+        if(userInput === this.currentWordUpdate[letterPos]){
+            this.currentWordUpdate.splice(letterPos, 1, '_');
+            this.correctGuess[letterPos] = userInput; 
+            this.updateCorrectLetter ();
+            debugger;
+            var gameState = this.gameState();
+            if(gameState){
+                this.updateWin();
+                this.updatePicture();
+            }
+        }else{
+            if(this.inCorrectGuess.indexOf(userInput)===-1){
+                this.inCorrectGuess.push(userInput);
+                this.updateIncorrectLetter();
+                this.loss++;
+            }
         }
-        console.log(currentWord)
+     },
+     //end of letterCheck
+     updateCorrectLetter : function(){
+         var correct_guess = this.correctGuess;
+         var new_correct_guess = $("<h3>");
+         new_correct_guess.addClass("correct");
+         $('#correct').html(correct_guess);
+    },// end of updateCorrectLetter
+    
+    updateIncorrectLetter : function(){
+         var wrong_guess = this.inCorrectGuess;
+         var new_wrong_guess = $("<h3>");
+         new_wrong_guess.addClass("wrong");
+         $('#wrong').html(wrong_guess);
+    },//end of updateIncorrectLetter
 
-    }   
-
-    var settingGuessArray = function(){
-        var correct_guess = correctGuess;
-        var new_correct_guess = $("<h3>");
-        new_correct_guess.addClass("correct");
-        $('#correct').html(correct_guess);
-    }
-
-    var updateCorrectLetter = function(){
-        var correct_guess = correctGuess;
-        var new_correct_guess = $("<h3>");
-        new_correct_guess.addClass("correct");
-        $('#correct').html(correct_guess);
-    }
-    var updateWin = function(){
-         win++;
-         var newScore = win;
-         var newWin = $("<span>");
-         newWin.addClass("win");
-         $("#win").html(newScore);
-    }
-
-    var updatePicture =function(){
+    updateWin : function(){    
+        this.win++;
+        var newScore = this.win;
+        var newWin = $("<span>");
+        newWin.addClass("win");
+        $("#win").html(newScore);
+    }, //end of updateWin
+    
+    updatePicture: function(){
         var link =  masterArr[target]["pic"];
         var bandImage = $("<img>");
         bandImage.addClass("image");
         bandImage.attr("src", link);
         $(".image").html(bandImage);
-    }
-    var updateBand = function(){
-        var newText = masterArr[target]["band"]
-        var bandName = $("<h1>");
-        bandName.addClass("text");
-        $(".text").html(newText);
+    },
 
+    gameState : function(){
+       
+        return this.correctGuess.join("") === this.currentWord.join(""); 
     }
 
-    var updateIncorrectLetter = function(){
-        var wrong_guess = inCorrectGuess;
-        var new_wrong_guess = $("<h3>");
-        new_wrong_guess.addClass("wrong");
-        $('#wrong').html(wrong_guess);
-    }
 
-    var gameState = function(){
-      return correctGuess.join("") === masterArr[target]["name"]; 
-     }
 
-     var letterCheck = function(){
-        var userInput = String.fromCharCode(event.keyCode).toLowerCase(); 
-        var letterPos = currentWord.indexOf(userInput);
-        if(userInput === currentWord[letterPos]){
-            currentWord.splice(letterPos, 1, '_');
-            correctGuess[letterPos] = userInput; 
-            updateCorrectLetter ();
-        }else{
-            if(inCorrectGuess.indexOf(userInput)===-1){
-                inCorrectGuess.push(userInput);
-                updateIncorrectLetter();
-                loss++;
-            }
-        }
+}//////end of obj
 
-     }
+  
+  
+
+
+    // var updateBand = function(){
+    //     var newText = masterArr[target]["band"]
+    //     var bandName = $("<h1>");
+    //     bandName.addClass("text");
+    //     $(".text").html(newText);
+
+    // }
+
+   
+
+    // var gameState = function(){
+    //   return correctGuess.join("") === masterArr[target]["name"]; 
+    //  }
+
+   
         
     
     document.onkeyup = function(event){
+    if(obj.correctGuess.length === 0 && obj.inCorrectGuess.length === 0){
+        obj.setGame();
+        obj.settingGuessArray();
+        obj.letterCheck ();
+    }else{
+        obj.letterCheck ();  
+    }
+    /*
     var turnOver = gameState();  // return false the first time
     if(!turnOver){
         letterCheck ();
+        if(correctGuess.join("") === masterArr[target]["name"]){
+            updateWin();
+            updatePicture();
+            updateBand();
+            setGame();
+            updateCorrectLetter();
+            updateIncorrectLetter();
+        }
        debugger;
    }else{
        if(correctGuess.join("") === masterArr[target]["name"]){
-        updateWin();
-        updatePicture();
-        updateBand();
-        setGame();
-        updateCorrectLetter();
-        updateIncorrectLetter();
+        // updateWin();
+        // updatePicture();
+        // updateBand();
+        // setGame();
+        // updateCorrectLetter();
+        // updateIncorrectLetter();
+        alert("game is over")
        }
 
      }// end of else   
    
-    
+    */
  }//end of document.onkeyup
     
 
