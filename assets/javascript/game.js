befb93a0658a289bce2fb909d7af0ee8846e23fd
M L <ml@Ms-MacBook-Pro.home>
2018-10-13 20:45:14 -0400

    adding final toucehes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,36 +2,45 @@ $(document).ready(function(){
 
     var obj ={
         masterArr : [
-            { name: "yourloveisking", pic: "assets/images/king.png", band:"Your love is king"},
-             { name: "thesweetesttaboo", pic: "assets/images/taboo.png", band:"The Sweetest Taboo"},
-             { name: "paradise", pic: "assets/images/paradise.png", band:"Paradise"},
-            { name: "jezebel", pic: "assets/images/jezebel.png", band:"Jezebel"},
-            { name: "isitacrime", pic: "assets/images/crime.png", band:"Is It a Crime"},
-            { name: "likeatattoo", pic: "assets/images/tatoo.png", band:"like a Tattoo"},
-            { name: "pearls", pic: "assets/images/perls.png", band:"Pearls"},
-            { name: "noordinarylove", pic: "assets/images/noordinary.png", band:"No Ordinary Love"},
-            { name: "cherishtheday", pic: "assets/images/cherish.png", band:"Cherish the Day"},
-            { name: "kissoflife", pic: "assets/images/kiss.png", band:"kiss of Life"},
-            { name: "hangontoyourlove", pic: "assets/images/hangon.png", band:"Hang on to your Love"} 
-        ], // did not take account of spcace in letter check
+            { name: "your love is king", pic: "assets/images/king.png", band:"Your love is king", songName:"assets/songs/king.mp3"},
+            { name: "jezebel", pic: "assets/images/jezebel.png", band:"Jezebel", songName:"assets/songs/jezebel.mp3"},
+            {name: "kiss of life", pic: "assets/images/kiss.png", band:"kiss of Life", songName:"assets/songs/kiss.mp3"}
+           
+            //Note: these elements are not associated with any songs and therefore are comment it out but might be of use in the future
+            // { name: "thesweetesttaboo", pic: "assets/images/taboo.png", band:"The Sweetest Taboo"},
+            //  { name: "paradise", pic: "assets/images/paradise.png", band:"Paradise"},
+            // { name: "isitacrime", pic: "assets/images/crime.png", band:"Is It a Crime"},
+            // { name: "likeatattoo", pic: "assets/images/tatoo.png", band:"like a Tattoo"},
+            // { name: "pearls", pic: "assets/images/perls.png", band:"Pearls"},
+            // { name: "noordinarylove", pic: "assets/images/noordinary.png", band:"No Ordinary Love"},
+            // { name: "cherishtheday", pic: "assets/images/cherish.png", band:"Cherish the Day"},
+            // { name: "hangontoyourlove", pic: "assets/images/hangon.png", band:"Hang on to your Love"} 
+
+        ], 
         correctGuess : [],
         inCorrectGuess: [],
         currentWord : [], 
         currantWordUpdate: [],
-        loss : 10, // would like to set loss based on word length | this.loss =  this.currentWord + 5? 
+        loss : 11, // Nice to have: set loss based on word length | this.loss =  this.currentWord 
         win: 0,
-
+        
+        //set array and get random number for current guessing word
         setGame : function(){
-            
             target = Math.floor(Math.random() * this.masterArr.length)
             this.currentWord = this.masterArr[target]["name"].split("");
             this.currentWordUpdate =this.masterArr[target]["name"].split("");
             for(var i = 0; i< this.currentWord.length; i++){
-                this.correctGuess[i] = ' - ';
+                if(this.currentWord[i] === " "){
+                    this.correctGuess[i] = " ";
+                }else{
+                    this.correctGuess[i] = '-';
+                }
+               
             }
 
         }, //end of setGame 
-
+        
+        //render dashes on screen 
         settingGuessArray : function(){
             var correct_guess = this.correctGuess;
             var new_correct_guess = $("<h3>");
@@ -39,6 +48,8 @@ $(document).ready(function(){
             $('#correct').html(correct_guess);
             },
             //end of settingGuessArray
+             
+        //compare user input to current guessing word and update are made accordingly
         letterCheck: function(){
             var userInput = String.fromCharCode(event.keyCode).toLowerCase(); 
             var letterPos = this.currentWordUpdate.indexOf(userInput);
@@ -46,22 +57,39 @@ $(document).ready(function(){
                 this.currentWordUpdate.splice(letterPos, 1, '_');
                 this.correctGuess[letterPos] = userInput; 
                 this.updateCorrectLetter (this.correctGuess);
+                //if user has solved the guess make upade then reset
                 var gameState = this.gameState();
                 if(gameState){
                     this.updateWin();
                     this.updatePicture();
                     this.updateBand();
                     this.reset();
+                    this.playSong();
                 }
             }else{
                 if(this.inCorrectGuess.indexOf(userInput)===-1){
                     this.inCorrectGuess.push(userInput);
                     this.updateIncorrectLetter(this.inCorrectGuess);
                     this.updateLoss();
+                    //if loss is 0 end game and reset the game 
+                    if(this.loss === 0){
+                        //alert("The Correct guess was " + this.masterArr[target]["name"].toUpperCase() +" Try again");
+                        var lostmesg ="<p>The Correct guess was </p>" + this.masterArr[target]["name"] +"<p> Try again! </p>"
+                        var newText = lostmesg;
+                        bandName = $("<h1>");
+                        bandName.addClass("lossMsg");
+                        $(".lossMsg").html(newText);
+
+
+                        this.reset();
+
+                    }
                 }
             }
         },
         //end of letterCheck
+
+        //screen updates
         updateCorrectLetter : function(word){
             var correct_guess = word;//this.correctGuess;
             var new_correct_guess = $("<h3>");
@@ -100,6 +128,13 @@ $(document).ready(function(){
             $(".image").html(bandImage);
         },// end of updatePicture
 
+        playSong: function(){
+            var audioElement = document.createElement("audio");
+            audioElement.setAttribute("src", this.masterArr[target]["songName"]);
+            audioElement.play();
+
+        }, // end of playSong
+
         updateBand: function(){
             var newText = this.masterArr[target]["band"]
             bandName = $("<h1>");
@@ -107,13 +142,13 @@ $(document).ready(function(){
             $(".text").html(newText);
         },// end of updateBand
 
+        // check if user has not solved the current guess
         gameState : function(){
-            return this.correctGuess.join("") === this.currentWord.join(""); 
+            return this.correctGuess.join("").trim()=== this.currentWord.join("").trim(); 
         },// end of gameState
 
+        //reset game
         reset: function(){
-       
-       
         var resetCorrectWord = ' ';
         var resetIcorrectWord = ' ';
         this.updateCorrectLetter(resetCorrectWord);
